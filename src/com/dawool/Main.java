b21f7b098d770be12a8d499b1b09ccb8b52d00c6
dawoori <daul@kookmin.ac.kr>
2020-01-15 15:35:01 +0900

    bit 전체 탐색

--- src/com/dawool/Main.java
+++ src/com/dawool/Main.java
@@ -1,16 +1,27 @@
 package com.dawool;
 
-import java.io.*;
+import java.util.*;
 
-public class Main{
+class Main {
     public static void main(String[] args) {
-        MazeMaker mazeMaker = new MazeMaker();
-        String[] maze = new String[]{"...","...","..."};
-        int sR = 0;
-        int sC = 1;
-        int[] mR = {1,0,-1,0};
-        int[] mC = {0,1,0,-1};
-//        System.out.println(mR[3]);
-        System.out.println(mazeMaker.longestPath(maze,sR,sC,mR,mC));
+        Scanner sc = new Scanner(System.in);
+        int node = sc.nextInt(), line = sc.nextInt(), start = sc.nextInt();
+
+        ArrayList<Integer>[] A = new ArrayList[node];
+        for (int i=0;i<node;i++) A[i] = new ArrayList<>();
+
+        for (int i=0;i<line;i++) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            A[a-1].add(b-1);
+            A[b-1].add(a-1);
+        }
+
+        for (ArrayList i : A) {
+            Collections.sort(i);
+            System.out.println(i);
+        }
+
+        int[] dfs = new int[node];
+        int[] bfs = new int[node];
     }
 }
---
+++ src/com/dawool/NumberMagicEasy.java
@@ -0,0 +1,21 @@
+package com.dawool;
+
+import java.util.ArrayList;
+
+public class NumberMagicEasy {
+    public int theNumber(String answer) {
+//        int[] card1 = {1, 2, 3, 4, 5, 6, 7, 8};
+//        int[] card2 = {1, 2, 3, 4, 9, 10, 11, 12};
+//        int[] card3 = {1, 2, 5, 6, 9, 10, 13, 14};
+//        int[] card4 = {1, 3, 5, 7, 9, 11, 13, 15};
+//
+//        ArrayList<Integer> num = new ArrayList<>();
+        int[] nums = new int[4];
+        for (int i=0;i<4;i++) {
+            if (answer.charAt(i) == 'Y') nums[i] = 1;
+            else nums[i] = 0;
+        }
+        int number = 16 - nums[0]*8 - nums[1]*4 - nums[2]*2 - nums[3];
+        return number;
+    }
+}
