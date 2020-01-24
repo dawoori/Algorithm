b8e48ba662c82d2214a08822d86c5300841f5050
dawoori <daul@kookmin.ac.kr>
2020-01-24 20:42:53 +0900

    Brute Force / acmicpc.net 7568

--- src/com/dawool/Main.java
+++ src/com/dawool/Main.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numOfCards = sc.nextInt(), maxSum = sc.nextInt();
+        int people = sc.nextInt();
 
-        int[] cards = new int[numOfCards];
+        int[] kg = new int[people];
+        int[] cm = new int[people];
+        int[] rank = new int[people];
 
-        for (int i = 0; i < numOfCards; i++) {
+        for (int i = 0; i < people; i++) {
+            kg[i] = sc.nextInt();
+            cm[i] = sc.nextInt();
+        }
 
+        for (int i = 0; i < people; i++) {
+            rank[i] = 1;
+            for (int j = 0; j < people; j++) {
+                if (kg[j] > kg[i] && cm[j] > cm[i]) rank[i]++;
+            }
+            System.out.printf("%d ", rank[i]);
         }
     }
 }
