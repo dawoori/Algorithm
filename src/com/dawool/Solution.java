80a3efca1067bed2baea3ee0212461ca718f1522
dawoori <daul@kookmin.ac.kr>
2020-02-05 23:07:11 +0900

    BufferedReader / BufferedWriter

--- src/com/dawool/Solution.java
+++ src/com/dawool/Solution.java
@@ -1,22 +1,63 @@
 package com.dawool;
 
-import java.util.HashMap;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'findNumber' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER k
+     */
+
+    public static String findNumber(List<Integer> arr, int k) {
+        // Write your code here
+
+    }
+
+}
 
 public class Solution {
-    public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> nam = new HashMap<>();
-        for (int i = 0; i < participant.length; i++) {
-            int alr = 0;
-            String par = participant[i];
-            if (nam.containsKey(par)) alr = nam.get(par);
-            nam.put(par, alr);
-        }
-        for (int i = 0; i < completion.length; i++) {
-            nam.put(completion[i], nam.get(completion[i]) - 1);
-        }
-        for (String key : nam.keySet()) {
-            if (nam.get(key) == 1) return key;
-        }
-        return "";
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, arrCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.findNumber(arr, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
