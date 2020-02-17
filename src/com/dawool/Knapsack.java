9ab305228b39f87f6b25ec33788e8a713958e929
dawoori <daul@kookmin.ac.kr>
2020-02-17 14:20:34 +0900

    knapsack 메모화

--- src/com/dawool/Knapsack.java
+++ src/com/dawool/Knapsack.java
@@ -4,13 +4,12 @@ public class Knapsack {
     int[] weight = {3, 4, 1, 2, 3};
     int[] value = {2, 3, 2, 3, 6};
     int maxWeight = 10;
-    int maxValue = 0;
+    int[][] dp = new int[6][11];
 
-    void knapsack(int n, int w, int p) {
-        if (w > maxWeight) return;
-        maxValue = Math.max(maxValue, p);
-        if (n >= weight.length) return;
-        knapsack(n + 1, w, p);
-        knapsack(n + 1, w + weight[n], p + value[n]);
+    int knapsack(int n, int w) {
+        if (w > maxWeight) return -1 * value[n - 1];
+        if (n >= weight.length) return 0;
+        if (dp[n][w] > 0) return dp[n][w];
+        return dp[n][w] = Math.max(knapsack(n + 1, w), knapsack(n + 1, w + weight[n]) + value[n]);
     }
 }
--- src/com/dawool/Main.java
+++ src/com/dawool/Main.java
@@ -1,12 +1,22 @@
 package com.dawool;
 
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.LinkedList;
 
 class Main {
     public static void main(String[] args) {
+        int[][] triangle = {{1}, {1, 2}, {1, 2, 3}};
+        System.out.println(triangle.length);
+        System.out.println(triangle[0].length);
+        System.out.println(triangle[1].length);
+        System.out.println(triangle[2].length);
+
+        System.out.println(Arrays.toString(triangle[0]));
+        System.out.println(Arrays.toString(triangle[1]));
+        System.out.println(Arrays.toString(triangle[2]));
+
         Knapsack knapsack = new Knapsack();
-        knapsack.knapsack(0,0,0);
-        System.out.println(knapsack.maxValue);
+
+        System.out.println(knapsack.knapsack(0, 0));
     }
 }
