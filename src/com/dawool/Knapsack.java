79742339685e7cf48dedab603de2609d2f6da1ed
dawoori <daul@kookmin.ac.kr>
2020-02-17 14:41:13 +0900

    knapsack DP

--- src/com/dawool/Knapsack.java
+++ src/com/dawool/Knapsack.java
@@ -4,7 +4,19 @@ public class Knapsack {
     int[] weight = {3, 4, 1, 2, 3};
     int[] value = {2, 3, 2, 3, 6};
     int maxWeight = 10;
+    int maxValue = 0;
     int[][] dp = new int[6][11];
+
+    void dp() {
+        for (int i = 0; i < weight.length; i++) {
+            for (int j = 0; j < maxWeight; j++) {
+                if (j + weight[i] <= maxWeight) {
+                    dp[i + 1][j + weight[i]] = Math.max(dp[i + 1][j + weight[i]], dp[i][j] + value[i]);
+                    maxValue = Math.max(dp[i + 1][j + weight[i]], maxValue);
+                }
+            }
+        }
+    }
 
     int knapsack(int n, int w) {
         if (w > maxWeight) return -1 * value[n - 1];
