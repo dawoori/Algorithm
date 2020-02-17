d5ea68241b1d57ce97eb7eaa0b3098f57d99caa4
dawoori <daul@kookmin.ac.kr>
2020-02-17 15:03:18 +0900

    knapsack DP

--- src/com/dawool/Knapsack.java
+++ src/com/dawool/Knapsack.java
@@ -5,11 +5,11 @@ public class Knapsack {
     int[] value = {2, 3, 2, 3, 6};
     int maxWeight = 10;
     int maxValue = 0;
-    int[][] dp = new int[6][11];
+    int[][] dp = new int[weight.length][maxWeight + 1];
 
     void dp() {
         for (int i = 0; i < weight.length; i++) {
-            for (int j = 0; j < maxWeight; j++) {
+            for (int j = 0; j <= maxWeight; j++) {
                 if (j + weight[i] <= maxWeight) {
                     dp[i + 1][j + weight[i]] = Math.max(dp[i + 1][j + weight[i]], dp[i][j] + value[i]);
                     maxValue = Math.max(dp[i + 1][j + weight[i]], maxValue);
--- src/com/dawool/Main.java
+++ src/com/dawool/Main.java
@@ -5,18 +5,13 @@ import java.util.LinkedList;
 
 class Main {
     public static void main(String[] args) {
-        int[][] triangle = {{1}, {1, 2}, {1, 2, 3}};
-        System.out.println(triangle.length);
-        System.out.println(triangle[0].length);
-        System.out.println(triangle[1].length);
-        System.out.println(triangle[2].length);
-
-        System.out.println(Arrays.toString(triangle[0]));
-        System.out.println(Arrays.toString(triangle[1]));
-        System.out.println(Arrays.toString(triangle[2]));
-
         Knapsack knapsack = new Knapsack();
-
-        System.out.println(knapsack.knapsack(0, 0));
+        knapsack.dp();
+        System.out.println(Arrays.toString(knapsack.dp[0]));
+        System.out.println(Arrays.toString(knapsack.dp[1]));
+        System.out.println(Arrays.toString(knapsack.dp[2]));
+        System.out.println(Arrays.toString(knapsack.dp[3]));
+        System.out.println(Arrays.toString(knapsack.dp[4]));
+        System.out.println(Arrays.toString(knapsack.dp[5]));
     }
 }
