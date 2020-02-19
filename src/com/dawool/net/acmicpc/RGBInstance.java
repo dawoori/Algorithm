62fd046a8089d415c76c1abe570b9fdf2bf710a5
dawoori <daul@kookmin.ac.kr>
2020-02-19 14:47:03 +0900

    dp

--- src/com/dawool/net/acmicpc/RGBInstance.java
+++ src/com/dawool/net/acmicpc/RGBInstance.java
@@ -4,21 +4,13 @@ package com.dawool.net.acmicpc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class RGBInstance {
-
-    static int[] rgb;
-    static int n;
-    static int[][] cost;
-    static int minCost;
-    static int[] tempCost;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        cost = new int[n][3];
-        tempCost = new int[n];
+        int n = Integer.parseInt(br.readLine());
+        int[][] cost = new int[n][3];
+        int[][] dp = new int[n][3];
 
         for (int i = 0; i < n; i++) {
             String[] s = br.readLine().split(" ");
@@ -27,31 +19,16 @@ public class RGBInstance {
             cost[i][2] = Integer.parseInt(s[2]);
         }
 
-        rgb = new int[n];
-        dfs(0);
-        System.out.println(minCost);
-    }
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
 
-    static void dfs(int house) {
-        if (house == 0) {
-            for (int i = 0; i < 3; i++) {
-                rgb[0] = i;
-                tempCost[0] = cost[0][i];
-                dfs(1);
-            }
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2];
         }
-        else {
-            for (int i = 0; i < 3; i++) {
-                if (house > 0 && rgb[house - 1] == i) continue;
-                rgb[house] = i;
-                tempCost[house] = tempCost[house - 1] + cost[house][i];
-                if (house < n - 1) {
-                    dfs(house + 1);
-                    continue;
-                }
-                if (minCost == 0) minCost = tempCost[n - 1];
-                else minCost = Math.min(minCost, tempCost[n - 1]);
-            }
-        }
+
+        System.out.println(Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]));
     }
 }
