27818cbf9b7ce396998b4bc6fca9d97e7b5c8a30
dawoori <daul@kookmin.ac.kr>
2020-05-07 16:28:25 +0900

    정

--- src/com/dawool/net/acmicpc/WordSorting.java
+++ src/com/dawool/net/acmicpc/WordSorting.java
@@ -4,19 +4,30 @@ package com.dawool.net.acmicpc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class WordSorting {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int numN = Integer.parseInt(br.readLine());
-        String[] words = new String[numN];
-        for (int i = 0; i < numN; i++) {
-            words[i] = br.readLine();
+        ArrayList<String>[] words = new ArrayList[50];
+        for (int i = 0; i < 50; i++) {
+            words[i] = new ArrayList<String>();
         }
 
-        System.out.println(Arrays.toString(words));
+        for (int i = 0; i < numN; i++) {
+            String word = br.readLine();
+            if(!words[word.length() - 1].contains(word)) words[word.length() - 1].add(word);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            Collections.sort(words[i]);
+            for (int j = 0; j < words[i].size(); j++) {
+                System.out.println(words[i].get(j));
+            }
+        }
     }
 }
 
