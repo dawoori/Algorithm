a2ecc29b3a69a5c56cf328aa7dd55f6836f77067
dawoori <daul@kookmin.ac.kr>
2020-07-02 13:23:21 +0900

    0701 study

--- src/com/dawool/Main.java
+++ src/com/dawool/Main.java
@@ -1,11 +1,23 @@
 package com.dawool;
 
+import java.util.*;
+
 class Main {
 
     public static void main(String[] args) throws Exception {
-        Solution solution = new Solution();
+        ArrayList<Integer> pipes = new ArrayList<>();
 
-        int solution1 = solution.solution(new int[][] {{0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 0}, {0, 0, 1, 0, 0, 0}, {1, 0, 0, 1, 0, 1}, {0, 1, 0, 0, 0, 1}, {0, 0, 0, 0, 0, 0}});
-        System.out.println(solution1);
+        pipes.add(1);
+        pipes.add(2);
+        pipes.add(3);
+
+        for (int i = 0; i < pipes.size(); i++) {
+            pipes.set(i, pipes.get(i) + 1);
+        }
+        System.out.println(pipes);
+
+        System.out.println(pipes.get(pipes.size() - 1));
+        pipes.remove(pipes.size() - 1);
+        System.out.println(pipes);
     }
 }
--- src/com/dawool/Solution.java
+++ src/com/dawool/Solution.java
@@ -1,31 +1,31 @@
 package com.dawool;
 
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Stack;
+
 class Solution {
-    boolean[][] visited;
-    char[][] direction;
-    int[][] prop = new int[][]{{100, 500}, {100, 500}};
-    int[] vx = new int[]{1, -1, 0, 0};
-    int[] vy = new int[]{0, 0, -1, 1};
+    public int solution(String arrangement) {
+        int answer = 0;
+//        ()(((()())(())()))(())
+        Stack<Integer> pipes = new Stack<>();
 
-    public int solution(int[][] board) {
-        int numberN = board.length;
-        visited = new boolean[numberN][numberN];
-        int answer = Math.min(dfs(1, 0, 0, board), dfs(0, 1, 1, board));
+        for (int i = 0; i < arrangement.length(); i++) {
+            if (arrangement.charAt(i) == '(' && arrangement.charAt(i + 1) == ')') {
+//                razer
+                for (int j = 0; j < pipes.size(); j++) {
+                    pipes.set(j, pipes.get(j) + 1);
+                }
+                i++;
+            } else if (arrangement.charAt(i) == '(') {
+//                open
+                pipes.push(1);
+            } else {
+//                close
+                answer += pipes.pop();
+            }
+        }
+
         return answer;
-    }
-
-    public int dfs(int iCor, int jCor, int dir, int[][] board) { // i 방향: 0, j 방향: 1
-        if (visited[iCor][jCor] || board[iCor][jCor] == 1) return 0;
-
-        visited[iCor][jCor] = true;
-        int ret = 0;
-        if (iCor < board.length - 2) ret = dfs(iCor + 1, jCor, 0, board) + prop[dir][0];
-        if (iCor > 1) ret += dfs(iCor - 1, jCor, 0, board) + prop[dir][0];
-        if (jCor < board.length - 2) ret += dfs(iCor, jCor + 1, 1, board) + prop[dir][1];
-        if (jCor > 1) ret += dfs(iCor, jCor - 1, 1, board) + prop[dir][1];
-
-        visited[iCor][jCor] = false;
-
-        return ret + 200;
     }
 }
