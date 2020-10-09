c8dd1f0ebc59d1c2178de1abad2ffc4ca7bce6ae
dawoori <daul@kookmin.ac.kr>
2020-10-09 16:58:30 +0900

    201009

--- src/com/dawool/Main.java
+++ src/com/dawool/Main.java
@@ -1,8 +1,38 @@
 package com.dawool;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Stack;
+
 class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
+
+        int n = 45;
+        int m = 3;
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        while(true) {
+            stack.push(n % m);
+            n = n / m;
+            if (n == 0) {
+                break;
+            }
+        }
+
+
+        int number = 0;
+        int digit = 0;
+
+        while(!stack.isEmpty()) {
+            int k = stack.removeFirst() * (int) Math.pow(10, digit);
+            number += k;
+            System.out.println(k);
+            digit++;
+        }
+
+        System.out.println(number);
+        System.out.println(Math.pow(10, 2));
     }
 }
--- src/com/dawool/Solution.java
+++ src/com/dawool/Solution.java
@@ -1,5 +1,34 @@
 package com.dawool;
 
-class Solution {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        int ternary = 3;
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        while(true) {
+            stack.push(n % ternary);
+            n = n / ternary;
+            if (n == 0) {
+                break;
+            }
+        }
+
+        int number = 0;
+        int digit = 0;
+
+        while(!stack.isEmpty()) {
+            int k = stack.removeFirst() * (int) Math.pow(10, digit);
+            number += k;
+            System.out.println(k);
+            digit++;
+        }
+
+        return answer;
+    }
 }
