7de3a56a2efa19abb03c41e904e18fff2756c969
dawoori <daul@kookmin.ac.kr>
2020-11-27 17:22:42 +0900

    knapsack dp

--- src/com/dawool/Main.java
+++ src/com/dawool/Main.java
@@ -6,9 +6,9 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        ChessMetric chessMetric = new ChessMetric();
-        long a = chessMetric.howMany(3, new int[]{0, 0}, new int[]{1, 0}, 1);
+        int a = -1;
         System.out.println(a);
+        System.out.println(-a);
     }
+
 }
--- src/com/dawool/Solution.java
+++ src/com/dawool/Solution.java
@@ -1,7 +1,19 @@
 package com.dawool;
 
-import java.util.*;
+public class Solution {
+    int[][] dp;
 
-class Solution {
+    public int knapsack(int[] w, int[] v, int k) {
+        dp = new int[w.length][k + 1];
+        int answer = 0;
+        answer = Math.max(dfs(w, v, k, 0, 0), answer);
+        return answer;
+    }
 
+    int dfs(int[] w, int[] v, int k, int idx, int nowW) {
+        if (nowW > k) return -1 * v[idx - 1];
+        if (idx >= w.length) return 0;
+        if (dp[idx][nowW] > 0) return dp[idx][nowW];
+        return dp[idx][nowW] = Math.max(dfs(w, v, k, idx + 1, nowW), dfs(w, v, k, idx + 1, nowW + w[idx]) + v[idx]);
+    }
 }
